058d3f54b0095fe2905ebbfccd5fa53dbfcbbac7
Austin Brain <cybrainfoxy@gmail.com>
2025-05-09 16:34:38 +0000

    üpdate

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import soundfile as sf
 
 from dia.dia.model import Dia
 
-model = Dia.from_local("model/config.json", "model/dia-v0_1.pth")
+model = Dia.from_pretrained("nari-labs/Dia-1.6B", compute_dtype="float16")
 
 
 text = "[S1] Dia is an open weights text to dialogue model. [S2] You get full control over scripts and voices. [S1] Wow. Amazing. (laughs) [S2] Try it now on Git hub or Hugging Face."
